2e3b9293a6522425704cb171baed07f8aaa309a3
Mukund Raghav Sharma <mrsharma@andrew.cmu.edu>
2015-02-20 00:53:05 -0500

    Updated models

--- Alumni/models.py
+++ Alumni/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Alumni(models.Model):
+    user = models.OneToOneField(User)
     first_name = models.CharField(null = True, max_length = 100)
     last_name = models.CharField(null = True, max_length = 100)
     employer = models.CharField(null = True, max_length = 100)
@@ -9,12 +11,14 @@ class Alumni(models.Model):
     phone = models.CharField(null = True, max_length = 50)
     major = models.CharField(null = True, max_length = 100)
     bio = models.CharField(null = True, max_length = 500)
-    picture = models.FileField(required = False)
+    picture = models.FileField()
+    family = models.CharField(null = True, max_length = 100)
     email = models.EmailField()
     graduation_class = models.CharField(null = True, max_length = 100)
     hometown = models.CharField(null = True, max_length = 100)
     pledge_class = models.CharField(null = True, max_length = 15)
-    time_stamp = models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add = True)
+    updated_at = models.DateTimeField(auto_now_add = True)
 
     def __unicode__(self):
         return self.first_name + " " +  self.last_name
