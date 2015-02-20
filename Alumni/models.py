0fce21117db0900d9071e9ff07d92dc14dc86649
Mukund Raghav Sharma <mrsharma@andrew.cmu.edu>
2015-02-20 02:44:03 -0500

    Updated the models and added comments:

--- Alumni/models.py
+++ Alumni/models.py
@@ -1,20 +1,25 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+# Inbuilt Django User fields - 1. first_name
+#                              2. last_name
+#                              3. username
+#                              4. password 
+
 class Alumni(models.Model):
     user = models.OneToOneField(User)
-    employer = models.CharField(null = True, max_length = 100)
-    position = models.CharField(null = True, max_length = 100)
-    current_city = models.CharField(null = True, max_length = 50)
-    phone = models.CharField(null = True, max_length = 50)
-    major = models.CharField(null = True, max_length = 100)
-    bio = models.CharField(null = True, max_length = 500)
+    employer = models.CharField(blank = True, max_length = 100)
+    position = models.CharField(blank = True, max_length = 100)
+    current_city = models.CharField(blank = True, max_length = 50)
+    phone = models.CharField(blank = True, max_length = 50)
+    major = models.CharField(blank = True, max_length = 100)
+    bio = models.CharField(blank = True, max_length = 500)
     picture = models.FileField()
-    family = models.CharField(null = True, max_length = 100)
+    family = models.CharField(blank = True, max_length = 100)
     email = models.EmailField()
-    graduation_class = models.CharField(null = True, max_length = 100)
-    hometown = models.CharField(null = True, max_length = 100)
-    pledge_class = models.CharField(null = True, max_length = 15)
+    graduation_class = models.CharField(blank = True, max_length = 100)
+    hometown = models.CharField(blank = True, max_length = 100)
+    pledge_class = models.CharField(blank = True, max_length = 15)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now_add = True)
 
