5a14e31364e11f3b606b4656d57a58da00f86beb
Mukund Raghav Sharma <mrsharma@andrew.cmu.edu>
2015-02-21 11:16:34 -0500

    Included information about setting up the postgresql

--- Alumni/setup.sh
+++ Alumni/setup.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/bash
+
+# echo "Create Database"
+# sudo -u postgres createuser -s $USER
+# createdb -U $USER --locale=en_US.utf-8 -E utf-8 -O $USER yournewdb -T template0
+
+# dependencies #
+# sudo ARCHFLAGS="-Wno-error=unused-command-line-argument-hard-error-in-future" pip install psycopg2
+
+# Django settings.py
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#        'NAME': 'yournewdb',
+#        'USER': 'yourusername',
+#        'PASSWORD': 'yourpassword',
+#        'HOST': 'localhost',
+#        'PORT': '5432',
+#}
