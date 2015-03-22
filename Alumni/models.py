ace9d65a554a95760349cbeaf1a0bdd31b058f90
Mukund Raghav Sharma <mrsharma@andrew.cmu.edu>
2015-03-22 04:33:57 -0400

    Shhfid

--- Alumni/models.py
+++ Alumni/models.py
@@ -5,6 +5,23 @@ from django.db.models.signals import post_save
 from django.shortcuts import render, redirect
 import uuid
 
+class Family(models.Model):
+    name = models.CharField(max_length = 10, blank = True)
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+    def __str__(self):
+        return str(self.name)
+
+    def save(self, *args, **kwargs):
+            try:
+                existing = Family.objects.get(name = self.name)
+                self.id = existing.id
+            except PledgeClass.DoesNotExist:
+                pass
+            models.Model.save(self, *args, **kwargs)
+
 class PledgeClass(models.Model):
     season = models.CharField(max_length = 10, blank = True)
     year = models.CharField(max_length = 10, blank = True)
@@ -36,7 +53,8 @@ class Alumni(models.Model):
     position_description = models.CharField(blank = True, max_length = 500)
     picture = models.FileField(blank = True)
     company_logo = models.FileField(blank = True)
-    family = models.CharField(blank = True, max_length = 100)
+    family = models.ForeignKey(Family, blank = True, null = True)
+    #family = models.CharField(blank = True, max_length = 100)
     nickname = models.CharField(blank = True, max_length = 100)
     graduation_class = models.CharField(blank = True, max_length = 100)
     hometown = models.CharField(blank = True, max_length = 100)
