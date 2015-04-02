021d7b9300febd45b403f04a385bc998f2309b01
jc77743 <jc77743@gmail.com>
2015-04-02 17:42:00 -0400

    worked on editing

--- AlumniInfo/settings.py
+++ AlumniInfo/settings.py
@@ -44,10 +44,11 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'Alumni',
     #'social.apps.django_app.default',
-    'inplaceeditform',
-    'django_wysiwyg',
+    # 'django_wysiwyg',
     'easy_thumbnails',
     'image_cropping',
+    'inplaceeditform',
+    'inplaceeditform_extra_fields',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -69,6 +70,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
    'django.core.context_processors.tz',
    'django.core.context_processors.request',
    'django.contrib.messages.context_processors.messages',
+
    #'social.apps.django_app.context_processors.backends',
    #'social.apps.django_app.context_processors.login_redirect',
 
@@ -84,7 +86,6 @@ AUTHENTICATION_BACKENDS = (
 
 ROOT_URLCONF = 'AlumniInfo.urls'
 
-WSGI_APPLICATION = 'AlumniInfo.wsgi.application'
 
 LOGIN_URL = '/'
 
@@ -99,6 +100,18 @@ MEDIA_URL = '/Alumni/media/'
 INPLACEEDIT_EVENT = "click"
 INPLACE_ENABLE_CLASS = 'enable'
 INPLACEEDIT_EDIT_EMPTY_VALUE = 'Click to edit'
+
+# ADAPTOR_INPLACEEDIT = {'textarea': 'inplaceeditform_extra_fields.fields.AdaptorTinyMCEField',
+#                        #'textarea': 'inplaceeditform_extra_fields.fields.AdaptorSimpleTinyMCEField',
+#                        'image': 'inplaceeditform_extra_fields.fields.AdaptorImageThumbnailField',
+#                        'fk': 'inplaceeditform_extra_fields.fields.AdaptorAutoCompleteForeingKeyField',
+#                        'm2mcomma': 'inplaceeditform_extra_fields.fields.AdaptorAutoCompleteManyToManyField'}
+
+# ADAPTOR_INPLACEEDIT = {'auto_fk': 'inplaceeditform_extra_fields.fields.AdaptorAutoCompleteForeingKeyField',
+#                        'auto_m2m': 'inplaceeditform_extra_fields.fields.AdaptorAutoCompleteManyToManyField',
+#                        'image_thumb': 'inplaceeditform_extra_fields.fields.AdaptorImageThumbnailField',
+#                        'tiny': 'inplaceeditform_extra_fields.fields.AdaptorTinyMCEField',
+#                        'tiny_simple': 'inplaceeditform_extra_fields.fields.AdaptorSimpleTinyMCEField'}
 
 AUTH_PROFILE_MODULE = 'Alumni.Alumni'
 # Database
--- AlumniInfo/urls.py
+++ AlumniInfo/urls.py
@@ -4,5 +4,5 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('Alumni.urls')),
-    url(r'^inplaceeditform/', include('inplaceeditform.urls')),
+    (r'^inplaceeditform/', include('inplaceeditform.urls')),
 )
