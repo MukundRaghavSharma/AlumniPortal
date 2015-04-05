50a3a3dad70c58eb0649080cb47da586728bac3f
Mukund Raghav Sharma <mrsharma@andrew.cmu.edu>
2015-04-05 02:39:18 -0400

    Ashdshdjs

--- Alumni/static/Alumni/js/Families/boss.js
+++ Alumni/static/Alumni/js/Families/boss.js
@@ -7,7 +7,7 @@ function Boss() {
         data.addColumn('string', 'Manager');
         data.addColumn('string', 'ToolTip');
         data.addRows([
-	['<a href="/profile/14444"Mukund Sharma</a>',''',''],
+	[Mukund Sharma,'',''],
 
         ]);
 
