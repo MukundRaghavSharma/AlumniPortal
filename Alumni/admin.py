33f561e1a230889c1faaca5f557df2c3026d8e1f
Mukund Raghav Sharma <mrsharma@andrew.cmu.edu>
2015-04-10 02:05:25 -0400

    cfghujhygfdxf

--- Alumni/admin.py
+++ Alumni/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from Alumni.models import Alumni, PledgeClass
-from image_cropping import ImageCroppingMixin
+#from image_cropping import ImageCroppingMixin
 
 admin.site.register(Alumni)
 admin.site.register(PledgeClass)
