def0f660f16e728463587a83ef1af64261f3b463
Mukund Raghav Sharma <mrsharma@andrew.cmu.edu>
2015-04-10 01:07:06 -0400

    Updated urls

--- AlumniInfo/urls.py
+++ AlumniInfo/urls.py
@@ -3,15 +3,9 @@ from django.contrib import admin
 from django.conf.urls import patterns, include, url
 # from Alumni import views
 
-
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('Alumni.urls')),
     url(r'^ajaximage/', include('ajaximage.urls')),
     url(r'^inplaceeditform/', include('inplaceeditform.urls')),
-    url(r'^inplaceeditform/', include('inplaceeditform.urls')),
 )
-
-
-
-
